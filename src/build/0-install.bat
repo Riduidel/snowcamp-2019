8aa9e6c5d5f602e7ccc4e31deaeb4b3c8c5e3f76
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-01-18 21:31:51 +0100

    🔧 Utilisation de la configuration correcte pour Elastic (pour Kibana, j'ai des doutes)

--- src/build/0-install.bat
+++ src/build/0-install.bat
@@ -16,9 +16,9 @@ kubectl apply -f ../k8s/kafka/kafka-connect-ui.yml
 REM Install Elastic
 helm repo add elastic https://helm.elastic.co
 helm repo update
-helm install --namespace elastic --name elasticsearch stable/elasticsearch
+helm install stable/elasticsearch --namespace elastic --name elasticsearch --set imageTag=6.5.4
 REM (voir https://github.com/elastic/helm-charts/blob/6.5.2-alpha1/elasticsearch/README.md)
-helm install --namespace elastic  --name kibana elastic/kibana --set elasticSearchURL=http://elasticsearch-master.elastic.svc.cluster.local:9200
+helm install elastic/kibana --namespace elastic --name kibana --set imageTag=6.5.4,elasticsearchURL=http://elasticsearch-client.elastic.svc.cluster.local:9200
 REM (voir https://github.com/elastic/helm-charts/tree/6.5.2-alpha1/kibana)
 
 REM install weave scope
