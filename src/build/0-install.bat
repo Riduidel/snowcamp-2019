2c9a86bb5308b7f1aa2427a55c12297f9c096571
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-01-20 18:50:43 +0100

    🐛 Il faut utiliser kibana-oss quand on n'utilise pas xpack !

--- src/build/0-install.bat
+++ src/build/0-install.bat
@@ -18,7 +18,7 @@ helm repo add elastic https://helm.elastic.co
 helm repo update
 helm install stable/elasticsearch --namespace elastic --name elasticsearch --set imageTag=6.5.4
 REM (voir https://github.com/elastic/helm-charts/blob/6.5.2-alpha1/elasticsearch/README.md)
-helm install elastic/kibana --namespace elastic --name kibana --set imageTag=6.5.4,elasticsearchURL=http://elasticsearch-client.elastic.svc.cluster.local:9200
+helm install elastic/kibana --namespace elastic --name kibana --set imageTag=6.5.4,elasticsearchURL=http://elasticsearch-client.elastic.svc.cluster.local:9200,image=docker.elastic.co/kibana/kibana-oss
 REM (voir https://github.com/elastic/helm-charts/tree/6.5.2-alpha1/kibana)
 
 REM install weave scope
