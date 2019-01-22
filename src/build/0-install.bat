b8b8a2553621ff4dcf94580d8555b3000af5a853
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-01-22 10:57:10 +0100

    ⚗ Refactoring des noms des topics pour éviter une transformation ... qui ne marche pas :-/

--- src/build/0-install.bat
+++ src/build/0-install.bat
@@ -27,8 +27,8 @@ REM install weave scope
 helm install stable/weave-scope --namespace weave-scope --name weave-scope
 
 REM Install configmaps for application configuration
-kubectl create configmap kafka-config -n jx-production --from-literal=kafka.topic=sncfReaderProduction --from-literal=kafka.url=kafka-cp-kafka.kafka.svc.cluster.local:9092
-kubectl create configmap kafka-config -n jx-staging --from-literal=kafka.topic=sncfReaderStaging --from-literal=kafka.url=kafka-cp-kafka.kafka.svc.cluster.local:9092
+kubectl create configmap kafka-config -n jx-production --from-literal=kafka.topic=timesheets_production --from-literal=kafka.url=kafka-cp-kafka.kafka.svc.cluster.local:9092
+kubectl create configmap kafka-config -n jx-staging --from-literal=kafka.topic=timesheets_staging --from-literal=kafka.url=kafka-cp-kafka.kafka.svc.cluster.local:9092
 
 kubectl create namespace configuration
 kubectl create -f src\k8s\navitia.yaml  -n configuration
--- src/build/reference/sncf-reader/charts/preview/templates/configmap.tpl
+++ src/build/reference/sncf-reader/charts/preview/templates/configmap.tpl
@@ -1,6 +1,6 @@
 apiVersion: v1
 data:
-  kafka.topic: sncfReaderPreview
+  kafka.topic: timesheets_preview
   kafka.url: kafka-cp-kafka.kafka.svc.cluster.local:9092
 kind: ConfigMap
 metadata:
