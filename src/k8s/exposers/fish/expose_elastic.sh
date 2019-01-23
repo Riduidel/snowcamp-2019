c7dbe1d25f5297ef76273e528da40ef98fed9297
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-01-23 17:45:28 +0100

    🏗 Plus besoin d'exposer elastic : on a un ingress !

--- src/k8s/exposers/fish/expose_elastic.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/fish
-kubectl port-forward --namespace elastic (kubectl get pod --namespace elastic --selector="app=elasticsearch,release=elasticsearch,component=client" --output jsonpath='{.items[0].metadata.name}') 18180:9200
