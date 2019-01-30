4adf98dd4c22aaad1c583b0e06e55e68b41d740e
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-01-30 14:22:44 +0100

    🔖 Version presented at snowcamp !

--- src/build/delete.bat
+++ src/build/delete.bat
@@ -1,6 +1,10 @@
 REM immediatly delete cluster and all contained data
 gcloud container clusters delete snowcamp-2019 --quiet --zone=europe-west1-d
 
+REM delete the environments
+curl -X DELETE -H "Authorization: token %GIT_ACCESS_TOKEN%" https://api.github.com/repos/Zenika/environment-snowcamp-2019-production
+curl -X DELETE -H "Authorization: token %GIT_ACCESS_TOKEN%" https://api.github.com/repos/Zenika/environment-snowcamp-2019-staging
+
 REM delete the reader app
 curl -X DELETE -H "Authorization: token %GIT_ACCESS_TOKEN%" https://api.github.com/repos/Riduidel/snowcamp-2019-sncf-timesheet-reader
 curl -X DELETE -H "Authorization: token %GIT_ACCESS_TOKEN%" https://api.github.com/repos/Zenika/snowcamp-2019-sncf-timesheet-reader
@@ -8,7 +12,3 @@ curl -X DELETE -H "Authorization: token %GIT_ACCESS_TOKEN%" https://api.github.c
 REM delete the web ui
 curl -X DELETE -H "Authorization: token %GIT_ACCESS_TOKEN%" https://api.github.com/repos/Riduidel/snowcamp-2019-web-ui
 curl -X DELETE -H "Authorization: token %GIT_ACCESS_TOKEN%" https://api.github.com/repos/Zenika/snowcamp-2019-web-ui
-
-REM delete the environments
-curl -X DELETE -H "Authorization: token %GIT_ACCESS_TOKEN%" https://api.github.com/repos/Zenika/environment-snowcamp-2019-production
-curl -X DELETE -H "Authorization: token %GIT_ACCESS_TOKEN%" https://api.github.com/repos/Zenika/environment-snowcamp-2019-staging
